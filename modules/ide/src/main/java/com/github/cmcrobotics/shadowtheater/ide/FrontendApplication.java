ca44293d85797e4513ad84ff1ee8f7d11ed051e6
Brice Copy <brice.copy@cern.ch>
2018-06-25 16:43:03 +0200

    Attempt to use JPA - complains of non managed class in the model

--- modules/ide/src/main/java/com/github/cmcrobotics/shadowtheater/ide/FrontendApplication.java
+++ modules/ide/src/main/java/com/github/cmcrobotics/shadowtheater/ide/FrontendApplication.java
@@ -2,11 +2,16 @@ package com.github.cmcrobotics.shadowtheater.ide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.map.repository.config.EnableMapRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.data.map.repository.config.EnableMapRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
-@EnableMapRepositories("com.github.chibyhq.store.model.repositories")
+@EnableJpaRepositories("com.github.chibyhq.store.model.repositories")
+@ComponentScan(basePackages = { "com.github.chibyhq.*" })
+@EntityScan("com.github.chibyhq.playar.model")
 @EnableSpringDataWebSupport
 public class FrontendApplication {
 	
