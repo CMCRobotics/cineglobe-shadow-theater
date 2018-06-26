c179fe63fed0a564d1f771053687dbd222047be7
Brice Copy <brice.copy@cern.ch>
2018-06-26 12:09:39 +0200

    JPA tests

--- modules/ide/src/test/java/com/github/cmcrobotics/shadowtheater/ide/FrontendApplicationTests.java
+++ modules/ide/src/test/java/com/github/cmcrobotics/shadowtheater/ide/FrontendApplicationTests.java
@@ -1,12 +1,21 @@
 package com.github.cmcrobotics.shadowtheater.ide;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.github.chibyhq.playar.model.Application;
+import com.github.chibyhq.playar.model.LogEntry;
+import com.github.chibyhq.playar.model.RunSession;
 import com.github.chibyhq.store.model.repositories.ApplicationRepository;
+import com.github.chibyhq.store.model.repositories.LogEntryRepository;
 import com.github.chibyhq.store.model.repositories.RunSessionRepository;
 
 
@@ -19,9 +28,27 @@ public class FrontendApplicationTests {
 	
 	@Autowired
 	RunSessionRepository runSessionRepository;
-	
+		
 	@Test
 	public void contextLoads() {
+	    assertNotNull(applicationRepository);
+	    assertNotNull(runSessionRepository);
+	    Application app = Application.builder().environment(new HashMap<>()).build();
+	    applicationRepository.save(app);
+	    
+	    RunSession session = new RunSession();
+        session.setApplicationUUID(app.getUuid());
+        
+        LogEntry logEntry = LogEntry.builder().line("Log Entry Line").error(true).startedAt(new Date()).build();
+        session.getLogEntries().add(logEntry);
+        runSessionRepository.save(session);
+        
+        Application qApp = applicationRepository.findOne(app.getUuid());
+        assertNotNull(qApp);
+        
+        RunSession qSession = runSessionRepository.findOne(session.getUuid());
+        assertNotNull(qSession);
+        assertEquals(1, qSession.getLogEntries().size());
 	}
 
 }
